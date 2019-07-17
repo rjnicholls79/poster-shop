4bcf393f04a799402716610c82f7f64ccac7568b
Ross Nicholls <rjnicholls79@icloud.com>
2019-07-17 13:17:35 +0100

    Price buttons

--- public/script.js
+++ public/script.js
@@ -12,7 +12,34 @@ new Vue({
     methods: {
       addToCart(product){
         this.total += product.price;
-        this.cart.push(product);
+        let found = false;
+        for(let i = 0; i < this.cart.length; i++){
+          if(this.cart[i].id === product.id){
+            this.cart[i].qty++;
+            found = true;
+          }
+        }
+        if(!found){
+          this.cart.push({
+            id: product.id,
+            title: product.title,
+            price: product.price,
+            qty: 1,
+          });
+        }
+      },
+      inc(item){
+        item.qty++;
+        this.total += item.price;
+      },
+      dec(item){
+        item.qty--;
+        this.total -= item.price;
+      }
+    },
+    filters:{
+      currency(price){
+        return "£".concat(price.toFixed(2));
       }
     }
 });
