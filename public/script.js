15eff5189164bcd7b8220a44ef7085db128baeff
Ross Nicholls <rjnicholls79@icloud.com>
2019-07-17 08:40:56 +0100

    last change at work
    added some buttons, keys, and id's

--- public/script.js
+++ public/script.js
@@ -1,6 +1,17 @@
 new Vue({
   el: "#app",
   data:{
-    total: 0
-  }
+    total: 0,
+    products:[
+      {title: "Product 1", id: 1},
+      {title: "Product 2",id: 2},
+      {title: "Product 3",id: 3},
+    ],
+    cart:[],
+  },
+    methods: {
+      addToCart(product){
+        console.log(product.id);
+      }
+    }
 });
